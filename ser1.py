f2844e73b75c850f08d22b5b32fc48fca3b73d74
Cyb3rgP4nk <anonymuda64@gmail.com>
2019-11-22 13:52:58 +0100

    Create ser1.py

---
+++ ser1.py
@@ -0,0 +1,50 @@
+from time import sleep, strftime, time
+import serial
+import pynmea2
+import datetime
+#import pyscopg
+#import json
+import os
+#---------------------------------------#
+
+
+
+#setup the serial port is connected
+#conn = psycopg2.connect(database='mydb', user='user', password='user')
+#cur = conn.cursor()
+#cur.execute('''select * from port''')
+#except psycopg2.Error as e:
+#    pass
+#conn.commit()
+#for row in cur.fetchall():
+#    print row[0]
+#cur.close()
+row = "/dev/ttyS0"
+ser = serial.Serial(row, baudrate = 38400, timeout = 0.5)
+#--------------------------------------------------------------#
+def Initialize():
+    global ser
+    try:
+        ser.isOpen()
+        print("\n Serial is open")
+    except:
+        print ("Error: serial Not Open")
+#-------------------------------------------#
+def Reader():
+    global ser
+    if (ser.isOpen()):
+        try:
+            x = ser.readline()
+            if x [0:6] == '$IIMTW':
+                parsed_line = pynmea2.parse(x)
+                temperature_reading = parsed_line.temperature
+                alpha = temperature_reading
+                meteo_data=[]
+                meteo_data.append(alpha)
+                print (meteo_data)
+                x = (x)
+                return x
+        except:
+            return "unable to print"
+    else:
+        return "cannot open serial port"
